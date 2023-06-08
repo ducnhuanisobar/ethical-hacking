476e3e8027c7a6428693f084cc526086d38e828d
Nguyen Duc Nhuan <Nhuan.Nguyen@merkle.com>
2023-06-08 16:28:33 +0700

    Update injection script

--- public/javascripts/injection.js
+++ public/javascripts/injection.js
@@ -1,4 +1,22 @@
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
 document.addEventListener("DOMContentLoaded", function(event) {
     //do work
     alert('Injection script running');
-  });
+    let dwsid = getCookie("dwsid");
+    alert("Cookies dwsid:"  + dwsid);
+});
